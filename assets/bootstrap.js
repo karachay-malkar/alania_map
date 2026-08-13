f021fa3aca20d51a06d2bb2b5e20a0b89ef3cf99
Alan Map Builder <actions@users.noreply.github.com>
2026-08-13 09:44:53 +0000

    Build Alan Map 7.0.25 rotated rectangle crop

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const RELEASE = '7.0.25-r1';
+  const RELEASE = '7.0.25-r2';
   const baseUrl = new URL('.', document.currentScript.src);
   const assetUrl = (name) => {
     const url = new URL(name, baseUrl);
